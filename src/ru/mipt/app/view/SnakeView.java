09adae66fde7b2d29ea5130c0ab28b11d2530cea
Maria Nosareva <mashunya_19-97@mail.ru>
2018-08-09 23:37:09 +0300

    snake draw function

--- src/ru/mipt/app/view/SnakeView.java
+++ src/ru/mipt/app/view/SnakeView.java
@@ -1,11 +1,13 @@
 package ru.mipt.app.view;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import ru.mipt.app.model.GameModel;
 import ru.mipt.app.model.Point;
 
 import java.util.List;
 
-public class SnakeView extends GeneralView {
+public class SnakeView {
 
     private GraphicsContext gc;
 
@@ -16,6 +18,40 @@ public class SnakeView extends GeneralView {
     private List<Point> nextTail;
 
     public SnakeView(GraphicsContext gc, Point head, List<Point> tail) {
+        this.gc = gc;
+        this.currTail = tail;
+        this.nextTail = tail;
+        this.currHead = head;
+        this.nextHead = head;
+    }
 
+    public void setNextBody(Point head, List<Point> tail) {
+        this.nextHead = head;
+        this.nextTail = tail;
+    }
+
+    public GraphicsContext draw() {
+
+        GameModel model = GameModel.getInstance();
+        int radius = model.getpointRadius();
+
+        if (this.currHead == this.nextHead) {
+            if (this.currTail == this.nextTail) {
+                this.gc.setFill(Color.LAWNGREEN);
+                this.gc.fillOval(this.currHead.getX(), this.currHead.getY(), radius, radius);
+                for (Point point: this.currTail) {
+                    this.gc.fillOval(point.getX(), point.getY(), radius, radius);
+                }
+            }
+            return this.gc;
+        }
+
+        Point last = currTail.get(currTail.size() - 1); // maybe first??!
+
+        this.gc.clearRect(last.getX(), last.getY(), radius, radius);
+        this.gc.setFill(Color.LAWNGREEN);
+        this.gc.fillOval(this.nextHead.getX(), this.nextHead.getY(), radius, radius);
+
+        return this.gc;
     }
 }
